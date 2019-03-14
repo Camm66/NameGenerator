fe587c1ae4265fca0871ae4faff1970a53059500
Cameron Morales <cam@Camerons-iMac.local>
2019-03-13 21:50:18 -0500

    feat: implemented functional model

--- Markov.py
+++ Markov.py
@@ -8,9 +8,10 @@ print("---------------------------------\n")
 gender = int(input("Type 0 for Females, 1 for Males:"))
 minLen = int(input("Enter the minimum length:"))
 maxLen = int(input("Enter the maximum length:"))
-order = int(input("Enter the order of the markov model:"))
-order = int(input("Enter the number of names to be generated: "))
+#order = int(input("Enter the order of the markov model:"))
+number = int(input("Enter the number of names to be generated: "))
 '''
 
 nameGenerator = NameGenerator()
-nameGenerator.generateMap()
+#nameGenerator.generateNames(gender, number, minLen, maxLen)
+nameGenerator.generateNames(0, 10, 4, 8)
--- NameGenerator.py
+++ NameGenerator.py
@@ -1,19 +1,20 @@
-
+import random
+from numpy.random import choice
 
 class NameGenerator:
     def __init__(self):
         # List of imported names
         self.boyNames = []
         self.girlNames = []
-        # Dictionary key == letter combination, value == letter count
-        self.boyCount = {}
-        self.girlCount = {}
+        # Markov models
+        self.boyModel = {}
+        self.girlModel = {}
         # Initialize Markov Process
         self.generateMap()
 
     def generateMap(self):
         self.importNames()
-        self.generateCount()
+        self.generateModel()
 
     def importNames(self):
         try:
@@ -29,37 +30,96 @@ class NameGenerator:
         finally:
             f.close()
 
-    def generateCount(self):
-        states = dict()
-        for item in self.boyNames:
-            for i in range(len(item) + 1):
-                for j in range(i):
-                    if item[j:i] in states:
-                        states[item[j:i]] += 1
+    def generateModel(self):
+
+        self.boyModel = self.parseNames(self.boyNames)
+        self.boyModel = self.calculateDistribution(self.boyModel)
+
+        for item in self.boyModel:
+            print("{} {}".format(item, self.boyModel[item]))
+
+        self.girlModel = self.parseNames(self.girlNames)
+        self.girlModel = self.calculateDistribution(self.girlModel)
+
+    def parseNames(self, inputList):
+        chain = 2
+        states = {}
+
+        for item in inputList:
+            s = "_" * chain + item
+            for i in range(0, len(item)):
+                prefix = s[i:i + chain]
+                suffix = s[i + chain]
+                if prefix in states:
+                    if suffix in states[prefix]:
+                        states[prefix][suffix] += 1
                     else:
-                        states[item[j:i]] = 1
-        self.boyCount = states
+                        states[prefix][suffix] = 1
+                else:
+                    states[prefix] = {}
+                    states[prefix][suffix] = 1
+            prefix = s[len(item): len(item) + chain]
+            suffix = "\n"
+            if prefix in states:
+                if suffix in states[prefix]:
+                    states[prefix][suffix] += 1
+                else:
+                    states[prefix][suffix] = 1
+            else:
+                states[prefix] = {}
+                states[prefix][suffix] = 1
+        return states
 
-        states = dict()
-        for item in self.girlNames:
-            for i in range(len(item) + 1):
-                for j in range(i):
-                    if item[j:i] in states:
-                        states[item[j:i]] += 1
-                    else:
-                        states[item[j:i]] = 1
-        self.girlCount = states
+    def calculateDistribution(self, inputModel):
+        count = 0
+        for element in inputModel:
+            arr = inputModel[element]
+            count = sum(arr.values())
+            for item in arr:
+                arr[item] = float(arr[item]) / float(count)
+            inputModel[element] = arr
+        return inputModel
 
-        print(self.boyCount)
-        print(self.girlCount)
+    def generateNames(self, gender, number, minLen, maxLen):
+        names = []
+        i = 0
+        while i < number:
+            result = self.newName(gender)
+            if minLen <= len(result) <= maxLen and result is not ' ' and result not in names:
+                names.append(result)
+                i += 1
 
-    def markovProcess(self, minLen, maxLen, gender):
-        pass
+        for name in names:
+            print(name)
 
+    def newName(self, gender):
+        prefix = "__"
+        suffix = ""
+        name = ""
 
-'''
-1) import individual names
-2) Get the count of each individual letter & letter combo
+        model = None
+        nameList = None
+        if gender is 1:
+            model = self.boyModel
+            nameList = self.boyNames
+        elif gender is 0:
+            model = self.girlModel
+            nameList = self.girlNames
 
+        while True:
+            suffix = choice(model[prefix].keys(), 1, model[prefix].values())
+        #    print(model[prefix].keys())
+        #    print(model[prefix].values())
+        #    print(suffix)
+            if suffix[0] == "\n":
+                break
+            else:
+                name = name + suffix[0]
+                prefix = prefix[1:] + suffix[0]
 
-'''
+        if name in nameList:
+            name = ' '
+        else:
+            name = name.capitalize()
+
+        return name
